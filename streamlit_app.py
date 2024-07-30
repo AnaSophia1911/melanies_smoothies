59d1efc3a6b465b006275c72700f05c06a46fdc4
AnaSophia1911 <124007502+AnaSophia1911@users.noreply.github.com>
2024-07-30 11:03:01 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 # Import python packages
+import requests
 import streamlit as st
 from snowflake.snowpark.functions import col
 
@@ -30,6 +31,9 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+	fv_df = st.dataframe(data=fruityvice_response.json()), use_container_width=True)
+
 
     #st.write(ingredients_string)
 
@@ -44,8 +48,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 	
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
-#new section for api
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response)
-fv_df = st.dataframe(data=fruityvice_response.json()), use_container_width=True)
